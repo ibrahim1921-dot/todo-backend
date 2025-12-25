d84c7bf73c222d52f938217a9611815ddabb1a9b
[Abdul-Sobur Ibrahim] <[abdulsobur@gmail.com]>
2025-12-25 09:06:50 +0000

    Updated the cors plicy to load client-url from .env

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOptions = {
     const allowedOrigins = [
       "http://localhost:5173",
       "http://localhost:5174",
-      CLIENT_URL,
+      process.env.CLIENT_URL,
     ].filter(Boolean); // Remove undefined values
 
     // Allow requests with no origin (like mobile apps, postman,curl, etc)
